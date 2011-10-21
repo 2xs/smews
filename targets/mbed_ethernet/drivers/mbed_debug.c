a06c773836f6b20c5d691e0cf26c2ea740bc6e6a
hauspie <hauspie@6e1a67c2-a8af-414f-9088-81f25f8c8380>
2011-10-21 12:20:47 +0000

    fixed missing includes
    git-svn-id: https://scm.gforge.inria.fr/svn/smews/trunk@236 6e1a67c2-a8af-414f-9088-81f25f8c8380

--- targets/mbed_ethernet/drivers/mbed_debug.c
+++ targets/mbed_ethernet/drivers/mbed_debug.c
@@ -1,23 +1,23 @@
 /*
 * Copyright or © or Copr. 2011, Michael Hauspie
-* 
+*
 * Author e-mail: michael.hauspie@lifl.fr
-* 
+*
 * This software is a computer program whose purpose is to design an
 * efficient Web server for very-constrained embedded system.
-* 
+*
 * This software is governed by the CeCILL license under French law and
-* abiding by the rules of distribution of free software.  You can  use, 
+* abiding by the rules of distribution of free software.  You can  use,
 * modify and/ or redistribute the software under the terms of the CeCILL
 * license as circulated by CEA, CNRS and INRIA at the following URL
-* "http://www.cecill.info". 
-* 
+* "http://www.cecill.info".
+*
 * As a counterpart to the access to the source code and  rights to copy,
 * modify and redistribute granted by the license, users are provided only
 * with a limited warranty  and the software's author,  the holder of the
 * economic rights,  and the successive licensors  have only  limited
-* liability. 
-* 
+* liability.
+*
 * In this respect, the user's attention is drawn to the risks associated
 * with loading,  using,  modifying and/or developing or reproducing the
 * software by the user in light of its specific status of free software,
@@ -25,10 +25,10 @@
 * therefore means  that it is reserved for developers  and  experienced
 * professionals having in-depth computer knowledge. Users are therefore
 * encouraged to load and test the software's suitability as regards their
-* requirements in conditions enabling the security of their systems and/or 
-* data to be ensured and,  more generally, to use and operate it in the 
-* same conditions as regards security. 
-* 
+* requirements in conditions enabling the security of their systems and/or
+* data to be ensured and,  more generally, to use and operate it in the
+* same conditions as regards security.
+*
 * The fact that you are presently reading this means that you have had
 * knowledge of the CeCILL license and that you accept its terms.
 */
@@ -42,6 +42,7 @@
 #ifdef MBED_DEBUG_MODE
 #include <rflpc17xx/drivers/ethernet.h>
 #include <rflpc17xx/debug.h>
+#include <rflpc17xx/printf.h>
 
 #include "mbed_debug.h"
 #include "protocols.h"
@@ -53,32 +54,32 @@ void mbed_dump_packet(rfEthDescriptor *d, rfEthRxStatus *s, int dump_contents)
     proto_eth_demangle(&eth, d->packet);
 
 
-    printf("= %02x:%02x:%02x:%02x:%02x:%02x -> ", 
-	   eth.src.addr[0], 
-	   eth.src.addr[1], 
-	   eth.src.addr[2], 
-	   eth.src.addr[3], 
-	   eth.src.addr[4], 
+    printf("= %02x:%02x:%02x:%02x:%02x:%02x -> ",
+	   eth.src.addr[0],
+	   eth.src.addr[1],
+	   eth.src.addr[2],
+	   eth.src.addr[3],
+	   eth.src.addr[4],
 	   eth.src.addr[5]);
 
-    printf("%02x:%02x:%02x:%02x:%02x:%02x : ", 
-	   eth.dst.addr[0], 
-	   eth.dst.addr[1], 
-	   eth.dst.addr[2], 
-	   eth.dst.addr[3], 
-	   eth.dst.addr[4], 
+    printf("%02x:%02x:%02x:%02x:%02x:%02x : ",
+	   eth.dst.addr[0],
+	   eth.dst.addr[1],
+	   eth.dst.addr[2],
+	   eth.dst.addr[3],
+	   eth.dst.addr[4],
 	   eth.dst.addr[5]);
-	   
+
     if (eth.type == PROTO_IP)
     {
 	uint32_t src = proto_ip_get_src(d->packet + PROTO_MAC_HLEN);
 	uint32_t dst = proto_ip_get_dst(d->packet + PROTO_MAC_HLEN);;
-	printf("IP %d.%d.%d.%d -> ", 
+	printf("IP %d.%d.%d.%d -> ",
 	       (src >> 24) & 0xFF,
 	       (src >> 16) & 0xFF,
 	       (src >> 8) & 0xFF,
 	       (src) & 0xFF);
-	printf("%d.%d.%d.%d ", 
+	printf("%d.%d.%d.%d ",
 	       (dst >> 24) & 0xFF,
 	       (dst >> 16) & 0xFF,
 	       (dst >> 8) & 0xFF,
