4c06e8a542993e199d2d4d34e7f10c5ddc79a7b0
Thomas Vantroys <thomas.vantroys@univ-lille1.fr>
2012-06-18 16:49:42 +0200

    Initial version of the ethernet driver for arduino

---
+++ targets/Arduino_Ethernet/drivers/nic.c
@@ -0,0 +1,45 @@
+#include "enc624J600.h"
+
+
+
+void nicInit(void)
+{
+	ENC624J600Init();
+	
+}
+
+//maj 04_05_12
+void nicSend(unsigned int len, unsigned char* ptrpacket)
+{
+	ENC624J600PacketSend(len, ptrpacket);
+	#ifdef DEBUG
+	stateSend();
+	#endif
+}
+//maj 04_05_12
+unsigned int nicPoll(unsigned int maxlen)
+{
+	return ENC624J600PacketReceive(maxlen);
+}
+
+void nicGetMacAddress(u08* macaddr)
+{
+	// read MAC address registers
+	*macaddr++ = ENC624J600Read(MAADR1H);
+	*macaddr++ = ENC624J600Read(MAADR1L);
+	*macaddr++ = ENC624J600Read(MAADR2H);
+	*macaddr++ = ENC624J600Read(MAADR2L);
+	*macaddr++ = ENC624J600Read(MAADR3H);
+	*macaddr++ = ENC624J600Read(MAADR3L);
+}
+//MAJ 01_06_12
+void nicSetMacAddress(u08* macaddr)
+{
+	// write MAC address
+	ENC624J600Write(MAADR1L, *macaddr++);
+	ENC624J600Write(MAADR1H, *macaddr++);
+	ENC624J600Write(MAADR2L, *macaddr++);
+	ENC624J600Write(MAADR2H, *macaddr++);
+	ENC624J600Write(MAADR3L, *macaddr++);
+	ENC624J600Write(MAADR3H, *macaddr++);
+}
