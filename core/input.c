81777d40769e96f5e2f514a32edcc88b4d786c96
Michael Hauspie <michael.hauspie@lifl.fr>
2012-09-07 16:48:31 +0200

    New fix of dump loops...

--- core/input.c
+++ core/input.c
@@ -1125,7 +1125,7 @@ char smews_receive(void) {
                                         output_handler = &http_404_handler;
                                         break;
                                     }
-                                    for(i = tmp_args_size-1 ; i; --i) {
+                                    for(i =0 ; i < tmp_args_size ; ++i) {
                                         ((unsigned char *)tmp_connection.protocol.http.args)[i] = 0;
                                     }
                                     continue;
