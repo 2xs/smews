2xs/smews

commit a4268e2907bde37cc5b59e430a107bb55e48e6fb

Reverse for loops to save code space
when possible, loops in the form
for (i= 0 ; i< size ; ++i)

has been changed to
for (i = size ; i ; --i)

core/link_layer_cache.c
@@ -71,7 +71,7 @@ static uint32_t _current_time = 0;
 static inline int _ip_equal(const unsigned char *ip1, const unsigned char *ip2)
 {
 	uint8_t i;
-	for (i = 0 ; i < 16 ; ++i)
+	for (i = 16 ; i ; --i)
 		if (ip1[i] != ip2[i])
 			return 0;
 	return 1;
@@ -79,7 +79,7 @@ static inline int _ip_equal(const unsigned char *ip1, const unsigned char *ip2)
 static inline int _ip_is_null(const unsigned char *ip)
 {
 	uint8_t i;
-	for (i = 0 ; i < 16 ; ++i)
+	for (i = 16 ; i ; --i)
 		if (ip[i])
 			return 0;
 	return 1;

core/input.c
@@ -1125,7 +1125,7 @@ char smews_receive(void) {
                                         output_handler = &http_404_handler;
                                         break;
                                     }
-                                    for(i = 0; i < tmp_args_size ; i++) {
+                                    for(i = tmp_args_size ; i; --i) {
                                         ((unsigned char *)tmp_connection.protocol.http.args)[i] = 0;
                                     }
                                     continue;

core/memory.c
@@ -37,9 +37,6 @@
 
 #include "connections.h"
 
-int debug_mem_buffers = 0;
-int debug_mem_infos = 0;
-
 struct free_bloc_s {
 	uint16_t size;
 	uint16_t to_next;
@@ -82,11 +79,6 @@ void mem_reset(void) {
 void *mem_alloc(uint16_t size) {
 	struct free_bloc_s *prev_free = NULL;
 	struct free_bloc_s *curr_free = first_free;
-
-	if (size == OUTPUT_BUFFER_SIZE)
-	    debug_mem_buffers++;
-	else if (size == sizeof(struct in_flight_infos_t))
-	    debug_mem_infos++;
 
 	/* memory allocation internals work with 32 bits granularity */
 	if(size % 4 > 0) {
@@ -134,12 +126,6 @@ static void chain_free_blocs(struct free_bloc_s *b1, struct free_bloc_s *b2) {
 /* free a bloc of at least size bytes (but multiple of 4).
  * Only use on free data. */
 void mem_free(void *ptr, uint16_t size) {
-
-    	if (size == OUTPUT_BUFFER_SIZE && ptr != NULL)
-	    debug_mem_buffers--;
-	else if (size == sizeof(struct in_flight_infos_t) && ptr != NULL)
-	    debug_mem_infos--;
-
 
 	if(ptr != NULL && size != 0) {
 		struct free_bloc_s *to_free = ptr;

core/output.c
@@ -106,8 +106,7 @@ struct curr_output_t curr_output;
 
 /* default DEV_PUT16 */
 #ifndef DEV_PUT16
-static void
-dev_put16 (unsigned char *word)
+static void dev_put16 (unsigned char *word)
 {
     DEV_PUT (word[1]);
     DEV_PUT (word[0]);
@@ -116,8 +115,7 @@ dev_put16 (unsigned char *word)
 #define DEV_PUT16(w) dev_put16(w)
 #endif
 
-static void
-dev_put16_val (uint16_t word)
+static void dev_put16_val (uint16_t word)
 {
     DEV_PUT (word >> 8);
     DEV_PUT (word);
@@ -127,8 +125,7 @@ dev_put16_val (uint16_t word)
 
 /* default DEV_PUT32 */
 #ifndef DEV_PUT32
-static void
-dev_put32 (unsigned char *dword)
+static void dev_put32 (unsigned char *dword)
 {
     DEV_PUT16 (dword + 2);
     DEV_PUT16 (dword);
@@ -138,8 +135,7 @@ dev_put32 (unsigned char *dword)
 #endif
 
 #ifdef IPV6
-static void
-dev_put32_val (uint32_t word)
+static void dev_put32_val (uint32_t word)
 {
     DEV_PUT (word >> 24);
     DEV_PUT (word >> 16);
@@ -171,8 +167,7 @@ dev_put32_val (uint32_t word)
 #define CONTENT_LENGTH_SIZE 6
 #define CHUNK_LENGTH_SIZE 4
 
-static uint8_t
-_service_headers_size (enum service_header_e service_header)
+static uint8_t _service_headers_size (enum service_header_e service_header)
 {
     uint8_t size = 0;
     switch (service_header)
@@ -194,8 +189,7 @@ _service_headers_size (enum service_header_e service_header)
 }
 
 /*-----------------------------------------------------------------------------------*/
-char
-out_c (char c)
+char out_c (char c)
 {
 #ifdef DISABLE_COROUTINES
     if (curr_output.dynamic_service_state == connection_terminated || curr_output.dynamic_service_state == none)
@@ -252,8 +246,7 @@ out_c (char c)
 }
 
 /*-----------------------------------------------------------------------------------*/
-void
-smews_send_packet (struct connection *connection)
+void smews_send_packet (struct connection *connection)
 {
     uint32_t index_in_file;
     uint16_t segment_length;
@@ -640,8 +633,7 @@ smews_send_packet (struct connection *connection)
 }
 
 /*-----------------------------------------------------------------------------------*/
-static inline int32_t
-able_to_send (const struct connection *connection)
+static inline int32_t able_to_send (const struct connection *connection)
 {
     if (!something_to_send (connection))
 	return 0;
@@ -682,8 +674,7 @@ static char gpip_output_buffer[OUTPUT_BUFFER_SIZE];
 #endif
 
 /*-----------------------------------------------------------------------------------*/
-char
-smews_send (void)
+char smews_send (void)
 {
     /* current handled connection */
     struct connection *active_connection = NULL;

targets/Funcard7/SConscript
@@ -37,10 +37,32 @@ env.Replace(CC = 'avr-gcc')
 env.Replace(AS = 'avr-as')
 env.Replace(AR = 'avr-ar')
 env.Replace(RANLIB = 'avr-ranlib')
-env.Append(CCFLAGS = '-fno-inline-small-functions -mmcu=at90s8515 -D __AVR_AT90S8515__ -DDAY=0x`date +%d` -DMONTH=0x`date +%m` -DYEAR=0x`date +%y`')
+env.Append(CCFLAGS = '-mmcu=at90s8515 -D __AVR_AT90S8515__ -DDAY=0x`date +%d` -DMONTH=0x`date +%m` -DYEAR=0x`date +%y`')
+
 env.Append(ASFLAGS = '-mmcu=at90s8515')
-env.Append(LINKFLAGS = '-mmcu=at90s8515 -D__AVR_AT90S8515__ -Wl,--relax')
+env.Append(LINKFLAGS = '-mmcu=at90s8515 -D__AVR_AT90S8515__')
 env.Append(CCFLAGS = '-mcall-prologues')
+
+
+# Size optimizations
+ # Saves 2 bytes
+env.Append(CCFLAGS = '-fno-inline-small-functions -fno-inline-functions')
+
+# This does not seem to save anything so disabled, kept in comments for future reference
+# env.Append(CCFLAGS = '-fno-reorder-blocks')
+# env.Append(CCFLAGS = '-fno-reorder-blocks-and-partition')
+# env.Append(CCFLAGS = '-fno-reorder-functions')
+# env.Append(CCFLAGS = '-fno-toplevel-reorder')
+
+# Saves 12 bytes
+env.Append(CCFLAGS = '-fno-move-loop-invariants')
+
+# Saves 8 bytes
+env.Append(CCFLAGS = '-fshort-enums')
+
+# Saves around 25 to 30 bytes (changes CALL and JMP to RCALL and RJMP when possible)
+env.Append(LINKFLAGS = '-Wl,--relax')
+
 
 binaryName = projectName + '.bin'
 env.Command(binaryName, elfName, 'avr-objcopy -R .eeprom -O binary $SOURCE $TARGET')

commit d7b6a7de6705ba6ab4cab8379ffaf7be473f8137

Fix a loop bug in link layer cache.
Bug was introduced by previous commit a4268e2.

core/link_layer_cache.c
@@ -71,17 +71,19 @@ static uint32_t _current_time = 0;
 static inline int _ip_equal(const unsigned char *ip1, const unsigned char *ip2)
 {
 	uint8_t i;
-	for (i = 16 ; i ; --i)
-		if (ip1[i] != ip2[i])
-			return 0;
+	for (i = 15 ; i ; --i)
+	    if (ip1[i] != ip2[i])
+		return 0;
 	return 1;
 }
 static inline int _ip_is_null(const unsigned char *ip)
 {
 	uint8_t i;
-	for (i = 16 ; i ; --i)
-		if (ip[i])
-			return 0;
+	for (i = 15 ; i ; --i)
+	{
+	    if (ip[i])
+		return 0;
+	}
 	return 1;
 }
 #define IP_EQUAL(ip1, ip2) _ip_equal(ip1,ip2)
@@ -93,6 +95,7 @@ static inline int _ip_is_null(const unsigned char *ip)
 #define COPY_IP(ip1, ip2) UI32(ip1) = UI32(ip2)
 #endif
 
+
 void add_link_layer_address(unsigned char *ip, unsigned char *link_layer)
 {
     uint8_t i, min_time_idx = 0;
@@ -102,25 +105,25 @@ void add_link_layer_address(unsigned char *ip, unsigned char *link_layer)
 
     for (i = 0 ; i < LINK_LAYER_CACHE_MAX_ENTRY ; ++i)
     {
-		if (_link_layer_table[i].timestamp < min_time)
-		{
-			min_time = _link_layer_table[i].timestamp;
-			min_time_idx = i;
-		}
-		if (IP_EQUAL(_link_layer_table[i].ip,ip) || IP_IS_NULL(_link_layer_table[i].ip))
-		{
-			memcpy(_link_layer_table[i].link_layer, link_layer, LINK_LAYER_ADDRESS_SIZE);
-			_link_layer_table[i].timestamp = NOW;
-			if (IP_IS_NULL(_link_layer_table[i].ip))
-			{
-				COPY_IP(_link_layer_table[i].ip,ip);
-			}
-			return;
-		}
+	if (_link_layer_table[i].timestamp < min_time)
+	{
+	    min_time = _link_layer_table[i].timestamp;
+	    min_time_idx = i;
+	}
+	if (IP_EQUAL(_link_layer_table[i].ip,ip) || IP_IS_NULL(_link_layer_table[i].ip))
+	{
+	    memcpy(_link_layer_table[i].link_layer, link_layer, LINK_LAYER_ADDRESS_SIZE);
+	    _link_layer_table[i].timestamp = NOW;
+	    if (IP_IS_NULL(_link_layer_table[i].ip))
+	    {
+		COPY_IP(_link_layer_table[i].ip,ip);
+	    }
+	    return;
+	}
     }
     /* every entry is used, remove the oldest one */
     COPY_IP(_link_layer_table[min_time_idx].ip,ip);
-	memcpy(_link_layer_table[min_time_idx].link_layer, link_layer, LINK_LAYER_ADDRESS_SIZE);
+    memcpy(_link_layer_table[min_time_idx].link_layer, link_layer, LINK_LAYER_ADDRESS_SIZE);
 }
 
 int get_link_layer_address(const unsigned char *ip, unsigned char *link_layer)

targets/mbed_ethernet/drivers/eth_output.c
@@ -35,7 +35,7 @@
 /*
   Author: Michael Hauspie <michael.hauspie@univ-lille1.fr>
   Created: 2011-09-02
-  Time-stamp: <2012-02-21 16:52:53 (hauspie)>
+  Time-stamp: <2012-09-07 10:01:45 (hauspie)>
 */
 #include <stdint.h>
 #include <string.h> /* memcpy */
@@ -93,10 +93,10 @@ int mbed_eth_fill_header(const unsigned char *ip)
 
    if (!get_link_layer_address(ip, dst_addr.addr))
    {
-      return 0;
+       return 0;
    }
    idx = 0;
-   /* Put the destination addre in the frame header */
+   /* Put the destination address in the frame header */
    PUT_MAC(ethernet_header, idx, dst_addr.addr);
    return 1;
 }